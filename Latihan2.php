973eab8f88e0ba11957b1b203dca2d3dd32d43da
Hilman Sulaeman <72838894+hilmansulaeman@users.noreply.github.com>
2021-02-19 23:20:43 +0700

    Add files via upload

---
+++ Latihan2.php
@@ -0,0 +1,27 @@
+<?php 
+/*
+Hilman Sulaeman
+203040029
+https://github.com/hilmansulaeman
+pertemuan 3 - 15 Februari 2021
+mempelajari struktur kendali PHP  
+Pengulangan dan Pengkodisian
+*/
+?>
+
+<?php 
+// Pengkodisian / Percabangan
+// if else
+// if else if else
+// ternary
+// switch
+
+$x = 30;
+if( $x < 20 ) {
+    echo "benar";
+} else if( $x == 20 ) {
+    echo "bingo!"
+}else {
+    echo "salah";
+}
+?>
